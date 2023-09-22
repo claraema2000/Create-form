4531e11b6b4854800e774cddd481f158eb857fd8
ClaraEma2000 <claraema2000@gmail.com>
2023-09-22 23:02:18 +0600

    Create a form bby using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
+import RefForm from './Components/RefForm/RefForm'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
+import StatefulForm from './Components/StatefulForm/StatefulForm'
 
 function App() {
 
   return (
     <>
       <h1>Vite + React</h1>
-      <SimpleForm></SimpleForm>
+      {/* <SimpleForm></SimpleForm> */}
+      {/* <StatefulForm></StatefulForm> */}
+      <RefForm></RefForm>
     </>
   )
 }
---
+++ src/_form_.js
@@ -0,0 +1,11 @@
+// Various way to create a form:
+
+// 1. use form tag, 
+//   onSubmit handler with event (e),
+//   from e access e.target.name-of-the-input-field.value
+
+// 2. controlled element: use individual field state for each inpur field
+
+// 3. controlled element: one object in a state holding all the input fields value
+
+4. uncontrolled element: 
