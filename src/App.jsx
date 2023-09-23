87aad000e4cf15ccc93252ffe678d8d4c03328fe
ClaraEma2000 <claraema2000@gmail.com>
2023-09-23 08:52:11 +0600

    Create a reusable form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,45 @@
 import './App.css'
 import HookForm from './Components/HookForm/HookForm'
 import RefForm from './Components/RefForm/RefForm'
+import ReusableForm from './Components/ReusableForm/ReusableForm'
 import SimpleForm from './Components/SimpleForm/SimpleForm'
 import StatefulForm from './Components/StatefulForm/StatefulForm'
 
 function App() {
 
+  // const handleSignUpSubmit = data => {
+  //   console.log('Sign up data: ', data);
+  // }
+  // const handleUpdateProfile = data => {
+  //   console.log('Update profile: ', data);
+  // }
+
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1>Form Master</h1>
       {/* <SimpleForm></SimpleForm> */}
       {/* <StatefulForm></StatefulForm> */}
       {/* <RefForm></RefForm> */}
-      <HookForm></HookForm>
+      {/* <HookForm></HookForm> */}
+      {/* <ReusableForm
+        formTitle={'Sign UP'}
+        handleSubmit={handleSignUpSubmit}>
+        <div>
+          <h2>Sign Up</h2>
+          <p>This is the most safe website you ever visit</p>
+        </div>
+
+      </ReusableForm>
+      <ReusableForm
+        formTitle={'Profile update'}
+        handleSubmit={handleUpdateProfile}
+        submitBtnText='Update'>
+        <div>
+          <h2>Update Profile</h2>
+          <p>You can update your profile from here</p>
+        </div>
+      </ReusableForm> */}
+
     </>
   )
 }
