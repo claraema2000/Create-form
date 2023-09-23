3245003cfcc783996b77b934ec15001b93fbd6bd
ClaraEma2000 <claraema2000@gmail.com>
2023-09-23 22:27:19 +0600

    explore more about context api

--- src/Components/Friend/Friend.jsx
+++ src/Components/Friend/Friend.jsx
@@ -1,7 +1,12 @@
+import { useContext } from "react";
+import { AssetContext } from "../Grandpa/Grandpa";
+
 const Friend = () => {
+    const gift = useContext(AssetContext);
     return (
         <div>
             <h2>Friend</h2>
+            <p>Get *{gift}* from grab</p>
         </div>
     );
 };
--- src/Components/Grandpa/Grandpa.jsx
+++ src/Components/Grandpa/Grandpa.jsx
@@ -1,25 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import Aunty from "../Aunty/Aunty";
 import Dad from "../Dad/Dad";
 import Uncle from "../Uncle/Uncle";
 import './Grandpa.css';
 
-export const AssetContext = createContext('Platinum');
+export const AssetContext = createContext('coal');
+export const MoneyContext = createContext(1000);
 
 const Grandpa = () => {
 
     const asset = 'Diamond';
+    const [money, setMoney] = useState(5000);
 
     return (
         <div className="grandpa">
             <h2>Grandpa</h2>
-            <AssetContext.Provider value="gold">
-                <section className="flex">
-                    <Dad asset={asset}></Dad>
-                    <Uncle asset={asset}></Uncle>
-                    <Aunty></Aunty>
-                </section>
-            </AssetContext.Provider>
+            <p>Net Money: {money}</p>
+            <MoneyContext.Provider value={[money, setMoney]}>
+                <AssetContext.Provider value="gold">
+                    <section className="flex">
+                        <Dad asset={asset}></Dad>
+                        <Uncle asset={asset}></Uncle>
+                        <Aunty></Aunty>
+                    </section>
+                </AssetContext.Provider>
+            </MoneyContext.Provider>
         </div>
     );
 };
